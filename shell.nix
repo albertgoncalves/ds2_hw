4d304c1733a9abce92fb5b71332a4a636891aefe
albertgoncalves <goncalves.albert@gmail.com>
2019-03-18 12:04:19 -0400

    works now on linux, need to test on mac

--- shell.nix
+++ shell.nix
@@ -3,19 +3,28 @@ with pkgs; mkShell {
     name = "Python";
     buildInputs = [ (python36.withPackages(ps: with ps;
                         [ pip
-                          csvkit
+                          pandas
+                          numpy
                           matplotlib
-                          descartes
+                          csvkit
                           flake8
+                          shapely       # | on linux, shapely fails via pip
+                          fiona         # | hence 'pip install --no-deps'...
+                          six           # | all geopandas deps included here
+                          pyproj        # | http://geopandas.org/install.html
+                          rtree         # |
+                          descartes     # |
+                          pysal         # |
                         ]
                     ))
+                    gdal
                     jq
                   ];
     shellHook = ''
         directory="src/deps/"
         requirements="requirements.txt"
         if [ ! -d $directory ]; then
-            pip install -t $directory -r $requirements
+            pip install --no-deps -t $directory -r $requirements
         fi
         export PYTHONPATH=$directory
         for subdir in data pngs; do
@@ -26,6 +35,8 @@ with pkgs; mkShell {
         if [ $(uname -s) = "Darwin" ]; then
             alias ls='ls --color=auto'
             alias ll='ls -al'
+        else
+            alias open="xdg-open"
         fi
         . .alias
     '';
