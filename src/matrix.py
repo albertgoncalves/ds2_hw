40af4c80f5c89744067530713a3069fcea0a9b36
albertgoncalves <goncalves.albert@gmail.com>
2019-03-20 22:19:00 -0400

    hmmm

--- src/matrix.py
+++ src/matrix.py
@@ -3,18 +3,26 @@
 from os import environ
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from utils import load_csv
+
+
+def colorbar(ax, mat):
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.5)
+    plt.colorbar(mat, cax=cax)
 
 
 def plot(data, directory):
     corr = data.corr()
     columns = corr.columns
+    ticks = range(len(columns))
     fig, ax = plt.subplots(figsize=(10, 10))
-    cax = ax.matshow(corr)
-    fig.colorbar(cax)
-    plt.xticks(range(len(columns)), columns, rotation=45, ha="left")
-    plt.yticks(range(len(columns)), columns)
+    mat = ax.matshow(corr, vmin=-1, vmax=1)
+    plt.xticks(ticks, columns, rotation=45, ha="left")
+    plt.yticks(ticks, columns)
+    colorbar(ax, mat)
     plt.tight_layout()
     plt.savefig("{}/pngs/matrix.png".format(directory))
     plt.close()
